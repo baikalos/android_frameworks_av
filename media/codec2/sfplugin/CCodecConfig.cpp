d2b5592b57aacc8cc5f4aa801ffeecc97db020c2
Houxiang Dai <houxiang.dai@mediatek.com>
2023-03-11 18:56:19 +0800

    Change config diff optional until requested
    The config diff just log, it may cost performance issue.
    
    Test:
    Media playback OK
    
    Bug: 258613450
    Change-Id: I3c641149b1f072a725b703a353eb9aa7a23f73c5

--- media/codec2/sfplugin/CCodecConfig.cpp
+++ media/codec2/sfplugin/CCodecConfig.cpp
@@ -1296,6 +1296,8 @@ bool CCodecConfig::updateFormats(Domain domain) {
     }
 
     ReflectedParamUpdater::Dict reflected = mParamUpdater->getParams(paramPointers);
+
+#if defined(LOG_NDEBUG) && !LOG_NDEBUG
     std::string config = reflected.debugString();
     std::set<std::string> configLines;
     std::string diff;
@@ -1315,6 +1317,7 @@ bool CCodecConfig::updateFormats(Domain domain) {
         ALOGD("c2 config diff is %s", diff.c_str());
     }
     mLastConfig.swap(configLines);
+#endif
 
     bool changed = false;
     if (domain & mInputDomain) {
